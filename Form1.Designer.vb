5fac17381e118692ef7df1021c5cc930374b7cf0
T~kay <102425578+Tkay6677@users.noreply.github.com>
2024-05-12 21:58:23 +0100

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,165 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.ListBox2 = New System.Windows.Forms.ListBox()
+        Me.ListBox3 = New System.Windows.Forms.ListBox()
+        Me.ListBox4 = New System.Windows.Forms.ListBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 21)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(221, 20)
+        Me.TextBox1.TabIndex = 0
+        Me.TextBox1.Text = "A+A+A+B"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(9, 5)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(61, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Expression:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(9, 67)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(42, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Output:"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(12, 83)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(255, 238)
+        Me.TextBox2.TabIndex = 2
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(273, 83)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(90, 225)
+        Me.ListBox1.TabIndex = 4
+        '
+        'ListBox2
+        '
+        Me.ListBox2.FormattingEnabled = True
+        Me.ListBox2.Location = New System.Drawing.Point(369, 83)
+        Me.ListBox2.Name = "ListBox2"
+        Me.ListBox2.Size = New System.Drawing.Size(90, 225)
+        Me.ListBox2.TabIndex = 5
+        '
+        'ListBox3
+        '
+        Me.ListBox3.FormattingEnabled = True
+        Me.ListBox3.Location = New System.Drawing.Point(465, 83)
+        Me.ListBox3.Name = "ListBox3"
+        Me.ListBox3.Size = New System.Drawing.Size(90, 225)
+        Me.ListBox3.TabIndex = 6
+        '
+        'ListBox4
+        '
+        Me.ListBox4.FormattingEnabled = True
+        Me.ListBox4.Location = New System.Drawing.Point(561, 83)
+        Me.ListBox4.Name = "ListBox4"
+        Me.ListBox4.Size = New System.Drawing.Size(90, 225)
+        Me.ListBox4.TabIndex = 7
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(273, 314)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(88, 38)
+        Me.Button1.TabIndex = 8
+        Me.Button1.Text = "Smart split"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(371, 314)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(88, 38)
+        Me.Button2.TabIndex = 9
+        Me.Button2.Text = "Group"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(239, 21)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(88, 38)
+        Me.Button3.TabIndex = 10
+        Me.Button3.Text = "Solve"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(725, 401)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ListBox4)
+        Me.Controls.Add(Me.ListBox3)
+        Me.Controls.Add(Me.ListBox2)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents ListBox2 As System.Windows.Forms.ListBox
+    Friend WithEvents ListBox3 As System.Windows.Forms.ListBox
+    Friend WithEvents ListBox4 As System.Windows.Forms.ListBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,242 @@
+﻿Public Class Form1
+    Dim expression As String = "B'+C+B+C+C'"
+    Dim input As String
+    Dim Delimiter As String
+    Dim newStr As String
+    Dim ExpressionArry() As String
+    Dim ORLaws As New Dictionary(Of String, String) From {{"A+1", "1"}, {"A+A", "A"}, {"A+A'", "1"}, {"A+0", "A"},
+                                                          {"A*1", "A"}, {"A*A", "A"}, {"A*A'", "0"}, {"A*0", "0"}}
+    Dim Variable As String
+    Dim HasValue As Boolean
+    Dim Operators As String
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        TextBox1.Text = expression
+
+       
+
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+      
+        SmartSplit()
+        For Each item In ListBox1.Items
+            TextBox2.Text = TextBox2.Text & item
+        Next
+        TextBox2.Text = TextBox2.Text & vbCrLf
+        Do Until "a" = "b"
+            Grouping()
+            ' MsgBox(ListBox2.Items.Count)
+            If ListBox2.Items.Count = 0 Then
+                '    MsgBox("END")
+                Exit Do
+            End If
+            For i As Integer = 0 To ListBox2.Items.Count - 1
+                Translate(i)
+                PerformOperation(0, i)
+                If HasValue Then
+                    Exit For
+                End If
+            Next
+        Loop
+
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        SmartSplit()
+        For Each item In ListBox1.Items
+            TextBox2.Text = TextBox2.Text & item
+        Next
+        TextBox2.Text = TextBox2.Text & vbCrLf
+    End Sub
+  
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Grouping()
+    End Sub
+
+    Private Sub ListBox2_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox2.SelectedIndexChanged
+        Translate(ListBox2.SelectedIndex)
+    End Sub
+
+    Private Sub ListBox3_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox3.SelectedIndexChanged
+        PerformOperation(0, ListBox2.SelectedIndex)
+    End Sub
+    Sub Grouping()
+        ListBox2.Items.Clear()
+
+        'For j As Integer = 0 To Operators.Length - 1
+        For i As Integer = 0 To ListBox1.Items.Count - 1
+            Operators = GetOperator(ListBox1.Items(i))
+
+            If ListBox1.Items(i) = Operators Then
+                ListBox2.Items.Add(ListBox1.Items(i - 1) & ListBox1.Items(i) & ListBox1.Items(i + 1))
+
+            End If
+
+        Next
+        '  Next
+    End Sub
+    Sub SmartSplit()
+        expression = TextBox1.Text
+        TextBox2.Text = Nothing
+        ReDim ExpressionArry(expression.Length - 1)
+        For i As Integer = 0 To expression.Length - 1
+
+            If expression.ToArray(i) = "'" Then
+                ' ListBox1.Items.RemoveAt(ListBox1.Items.Count - 1)
+                ExpressionArry(i - 1) = Nothing
+                input = expression.ToArray(i - 1) & expression.ToArray(i)
+            Else
+                input = expression.ToArray(i)
+            End If
+            ' ListBox1.Items.Add(input)
+            ExpressionArry(i) = input
+        Next
+        ListBox1.Items.Clear()
+        For Each item In ExpressionArry
+            LoadLB(item)
+        Next
+    End Sub
+    Sub LoadLB(item As String)
+        If Not item = Nothing Then
+            ListBox1.Items.Add(item)
+            'MsgBox("Hold")
+        End If
+    End Sub
+    Sub Translate(index As Integer)
+        Dim Bla() As String
+
+        ListBox3.Items.Clear()
+        If Not ListBox2.Items.Count = 0 Then
+            'For i As Integer = 0 To Operators.Length - 1 'ListBox2.Items.Count - 1
+            Operators = GetOperator(ListBox2.Items(index).ToString)
+            Bla = ListBox2.Items(index).ToString.Split(Operators)
+                If Bla(0) = Bla(1) Then
+                    Variable = Bla(0)
+                ListBox3.Items.Add("A" & Operators & "A")
+                ElseIf Bla(0).Contains(Bla(1)) Or Bla(1).Contains(Bla(0)) Then
+                    Variable = Bla(0)
+                ListBox3.Items.Add("A" & Operators & "A'")
+            ElseIf IsNumeric(Bla(0)) Or IsNumeric(Bla(1)) Then
+
+
+                If IsNumeric(Bla(0)) Then
+
+                    Select Case CInt(Bla(0))
+                        Case 0
+                            ListBox3.Items.Add("A" & Operators & 0)
+                            Variable = Bla(1)
+                        Case 1
+                            ListBox3.Items.Add("A" & Operators & 1)
+                            Variable = Bla(0)
+                    End Select
+                ElseIf IsNumeric(Bla(1)) Then
+
+
+                    Select Case CInt(Bla(1))
+                        Case 0
+                            ListBox3.Items.Add("A" & Operators & 0)
+                            Variable = Bla(1)
+                        Case 1
+                            ListBox3.Items.Add("A" & Operators & 1)
+                            Variable = Bla(0)
+                    End Select
+
+                End If
+
+
+
+            Else
+                ListBox3.Items.Add("A" & Operators & "B")
+            End If
+            End If
+            'Next
+
+
+    End Sub
+    Function GetOperator(text As String)
+        Dim op As String = Nothing
+        If text.Contains("+") Then
+            op = "+"
+        ElseIf text.Contains("*") Then
+            op = "*"
+        End If
+        Return op
+    End Function
+    Sub PerformOperation(index As Integer, index2 As Integer)
+
+        If ORLaws.ContainsKey(ListBox3.Items(index)) Then
+            HasValue = True
+            'MsgBox("boo")
+            'MsgBox(ORLaws(ListBox3.Items(index)) & IsNumeric(ORLaws(ListBox3.Items(index))))
+            If IsNumeric(ORLaws(ListBox3.Items(index))) Then
+                'MsgBox()
+                ListBox4.Items.Add(ORLaws(ListBox3.Items(index)))
+
+            Else
+                MsgBox(Variable)
+                ListBox4.Items.Add(Variable)
+
+            End If
+            AddRest(index2)
+            For Each item In ListBox1.Items
+                TextBox2.Text = TextBox2.Text & item
+            Next
+            TextBox2.Text = TextBox2.Text & vbCrLf
+            'Reset()
+        Else
+            HasValue = False
+            'MsgBox("No way")
+            For i As Integer = index2 * 2 To (index2 * 2) + 1
+                ListBox4.Items.Add(ListBox1.Items(i))
+            Next
+
+        End If
+
+    End Sub
+    Sub AddRest(index As Integer)
+        'MsgBox(index * 2)
+        If (index * 2) + 2 <> ListBox1.Items.Count - 1 Then
+            For i As Integer = (index * 2) + 3 To ListBox1.Items.Count - 1
+                ListBox4.Items.Add(ListBox1.Items(i))
+            Next
+
+        End If
+        Reset()
+        '  
+
+    End Sub
+    Sub Reset()
+        'Try
+       
+        ListBox1.Items.Clear()
+        For Each item In ListBox4.Items
+            LoadLB(item)
+        Next
+        ListBox4.Items.Clear()
+        'Grouping()
+        'Translate(0)
+        'ListBox4.Items.Clear()
+        'For i As Integer = 0 To ListBox3.Items.Count - 1
+        '    PerformOperation(i)
+        'Next
+        'Catch ex As Exception
+        'MsgBox(ex.ToString)
+        'End Try
+
+    End Sub
+    Function IsAlpha(character As String)
+        Dim result As Boolean = False
+        For i As Integer = 65 To 90
+            If Asc(character) = i Then
+                result = True
+            End If
+        Next
+        Return result
+    End Function
+
+   
+   
+   
+    
+   
+End Class
